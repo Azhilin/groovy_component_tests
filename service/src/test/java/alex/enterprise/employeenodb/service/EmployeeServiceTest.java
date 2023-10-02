df7086b3238451920f20c4ded0a5ab74cf0ca8a4
Oleksii <oleksii@gmail.com>
2023-10-02 10:48:22 +0200

    Added Unit Test and Jacoco plugin for coverage

---
+++ service/src/test/java/alex/enterprise/employeenodb/service/EmployeeServiceTest.java
@@ -0,0 +1,13 @@
+package alex.enterprise.employeenodb.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+  @Test
+  void clear() {
+    assertTrue(true);
+  }
+}
